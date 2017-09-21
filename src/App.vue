6e06abb66a5640cb18315a4f5e8262369a4a1edd
fmaldonados <felixmaldonado@unitec.edu>
2017-09-20 20:32:10 -0600

    ahora con webpack

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
+import Hello from './components/Hello'
 
 export default {
   name: 'app',
@@ -14,3 +15,14 @@ export default {
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
