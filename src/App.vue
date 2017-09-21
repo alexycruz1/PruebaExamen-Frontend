5c43ab05c91a38891498afbed70609a28a0505fd
fmaldonados <felixmaldonado@unitec.edu>
2017-09-20 21:03:17 -0600

    intento con browserify

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Hello from './components/Hello.vue'
 
 export default {
   name: 'app',
@@ -15,14 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
