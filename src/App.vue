e639ea6b60fe0d38658220c0989d1010753335a1
fmaldonados <felixmaldonado@unitec.edu>
2017-09-20 22:05:24 -0600

    navbar con search

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">JavaScript</a></li>
-        </ul>
+    <nav class='blue'>
+      <div class="row">
+        <div class="col s1 m1 l1">
+          <div class="nav-wrapper">
+            <a class="brand-logo">Prueba</a>
+          </div>
+        </div>
+        <div class="col s3 m3 l3">
+          <ul id="nav-mobile" >
+            <li><a>Sass</a></li>
+            <li><a>Components</a></li>
+            <li><a>JavaScript</a></li>
+          </ul>
+        </div>
+        <div class="input-field col s4 offset-s4">
+          <div class="col s1 m1 l1 ">
+            <i class="material-icons">search</i>
+          </div>
+          <div class="col s11 m11 l11 ">
+            <input id="search" type="search" required>
+          </div>
+        </div> 
       </div>
     </nav>
     <hello></hello>
